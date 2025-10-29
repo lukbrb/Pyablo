ddb4783ff544f53774b4e9049992df0ca93d47f5
Lucas Barbier-Goy <lucasbarbr@outlook.com>
2025-10-29 16:57:07 +0100

    update test file and renameit to example.py

--- example.py
+++ example.py
@@ -1,12 +1,14 @@
-import pydy
-import numpy as np
-import matplotlib.pyplot as plt
 import os
 
-r = pydy.XdmfReader()
-filename = '../examples/blast_3d.xmf'
+import matplotlib.pyplot as plt
+import numpy as np
+
+import pyablo
+
+r = pyablo.XdmfReader()
+filename = 'examples/blast.xmf'
 if not os.path.exists(filename):
-    print('ARG')
+    print(f"File {filename} does not exist.")
     exit(1)
 
 s = r.readSnapshot(filename)
@@ -15,7 +17,7 @@ s.print()
 y  = np.linspace(0.01, 0.99, 1000)
 xz = np.ones_like(y) * 0.5
 v = np.stack((xz.T, y.T, xz.T)).T
-rho = s.probeDensities(v)
+rho = s.probeDensity(v)
 plt.plot(y, rho, '-+')
 plt.show()
 
